569f46f88967012dd00114a3276378b7258c6c18
Tharindu <tharinduprabashwara71@gmail.com>
2023-12-19 21:34:09 +0530

    Update pagination_footer.php
    fix a issue in pagination which not show entry count when 5>

--- app/views/dashboards/common/pagination_footer.php
+++ app/views/dashboards/common/pagination_footer.php
@@ -6,26 +6,20 @@
                     <?php if( count($data[$current_page]) >= 5) {
 
                     echo ' 
-                    
-                            <select class="show-entries">
-                            <option value="5">5</option>
-                            <option value="10">10</option>
-                            <option value="15">15</option>
-                            </select>
+                    <select class="show-entries">
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                    </select>
 
-                        
-                            </span> of <span class="foot-number"><?php echo count($data[$current_page]); ?></span> entries</span>
+                    </span> of <span class="foot-number">' . count($data[$current_page]) . '</span> entries</span>
 
                     
-                            <div class="pagination-main">
-                               
-                              
-                                <ul class="pagination"></ul>
-                              
-        
-                            </div>
+                    <div class="pagination-main">
+                        <ul class="pagination"></ul>
+                    </div>
+                    ';
 
-                        ';
 
                     }else {
 
